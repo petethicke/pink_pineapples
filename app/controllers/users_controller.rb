83b886195faf579d745062726216a79fa13acb4d
Peter Thicke <peterbthicke@gmail.com>
2015-04-28 18:17:44 +0000

    added sessions functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   def new
     @user = User.new
   end
@@ -8,11 +8,12 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to movies_path
-    else
-      render :new
+        session[:user_id] = @user.id # auto log in
+        redirect_to movies_path
+      else
+        render :new
+      end
     end
-  end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
 
   resources :movies
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
