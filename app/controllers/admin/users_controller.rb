9645165626dea30ddadb0ebaf4d4d02c12d131f1
Peter Thicke <peterbthicke@gmail.com>
2015-04-29 01:11:48 +0000

    added method to determine whether or not a user is an admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,21 @@
 class Admin::UsersController < ApplicationController
+  
+  before_filter :ensure_admin
+
+  def new
+      @user = User.new
+  end
+
+  protected
+
+  def ensure_admin
+    if !current_user
+      flash[:alert] = 'You need to log in!'
+      redirect_to '/sessions/new'
+    elsif !current_user.admin
+      flash[:alert] = "You are not an admin!"
+      redirect_to '/'
+    end
+  end
+
 end
