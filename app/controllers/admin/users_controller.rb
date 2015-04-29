81499687ff1fd445d07a102be3146468cce88046
Peter Thicke <peterbthicke@gmail.com>
2015-04-29 01:43:27 +0000

    formatted admin index page to display all users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,8 +2,16 @@ class Admin::UsersController < ApplicationController
   
   before_filter :ensure_admin
 
+  def index
+    @users = User.all
+  end
+
   def new
-      @user = User.new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   protected
@@ -12,7 +20,7 @@ class Admin::UsersController < ApplicationController
     if !current_user
       flash[:alert] = 'You need to log in!'
       redirect_to '/sessions/new'
-    elsif !current_user.admin
+    elsif !current_user.admin 
       flash[:alert] = "You are not an admin!"
       redirect_to '/'
     end
